2e6c315770c3658b57d530e8a3cc10077866afdd
Nikolai Stanchev <nikolairadostinov@abv.bg>
2024-04-18 22:32:25 +0300

    book

---
+++ books/Program.cs
@@ -0,0 +1,15 @@
+﻿books book = new books();
+
+Console.Write("Въведете Книга:");
+
+book.title = Console.ReadLine();
+
+Console.Write("Въведете автор:");
+
+book.author = Console.ReadLine();
+
+Console.Write("Въведете година на издаване:");
+
+book.age = int.Parse(Console.ReadLine());
+
+book.writeal();
---
+++ books/book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class books
+{
+    public books()
+    {
+
+    public string author;
+    public string title;
+    public int age;
+    public void writeal()
+    {
+        Console.WriteLine($"Заглавието на книгата е  {title}  автор е {author} книгата е издадена  {age} ");
+
+    }
+
+    }
+}
+
+
