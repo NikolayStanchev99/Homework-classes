a40ee64080f76f94fcf10adbeac069dab59373f4
Nikolai Stanchev <nikolairadostinov@abv.bg>
2024-04-18 22:18:38 +0300

    Person

---
+++ Person/Program.cs
@@ -0,0 +1,15 @@
+﻿Person human = new Person();
+
+Console.Write("въведете име:");
+
+human.Name = Console.ReadLine();
+
+Console.Write("Въведете години:");
+
+human.age = int.Parse(Console.ReadLine());
+
+Console.Write("Въведете пол:");
+
+human.pol = Console.ReadLine();
+
+person.info();
---
+++ Person/humen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {
+
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+
+    public void info()
+    {
+        Console.WriteLine($"Вие се казвате {Name} и стена   на {age} години и полът вие {Gender}.");
+
+    }
+    } 
+}
+
