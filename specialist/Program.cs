14d74d16998b947b3e1de7118aaa1f66d6c40010
Nikolai Stanchev <nikolairadostinov@abv.bg>
2024-04-18 23:04:27 +0300

    specialist

---
+++ specialist/Program.cs
@@ -0,0 +1,15 @@
+﻿speshal specialist = new speshal();
+
+Console.Write("Въведете име:");
+
+specialist.name = Console.ReadLine();
+
+Console.Write("Въведете заплата:");
+
+specialist.salary = int.Parse(Console.ReadLine());
+
+Console.Write("Въведете одтел:");
+
+specialist.otdel = Console.ReadLine();
+
+specialist.info();
---
+++ specialist/speshal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class speshal
+{
+    public speshal()
+    {
+
+    public string name;
+    public int salary { get; set; }
+    public string otdel { get; set; }
+    public void info()
+    {
+        Console.WriteLine($"Специалистът {name}, със заплата {salary} от отдел {otdel}.");
+    }
+
+    }
+}
