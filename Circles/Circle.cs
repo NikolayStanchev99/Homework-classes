aadc3120a64f8ca628ad50d1c2caed44eba3b76d
Nikolai Stanchev <nikolairadostinov@abv.bg>
2024-04-18 22:58:55 +0300

    Circle

---
+++ Circles/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Circles
+{
+	public Circles()
+	{
+		
+    public double Radius { get; set; }
+
+    
+    public Circles(double radius)
+    {
+        Radius = radius;
+    }
+
+   
+    public double CalculateCircumference()
+    {
+        return 2 * Math.PI * Radius;
+    }
+
+    
+    public double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    }
+}
---
+++ Circles/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Circles circle1 = new Circles(5);
+
+
+double circumference = circle1.CalculateCircumference();
+double area = circle1.CalculateArea();
+
+Console.WriteLine($"Обиколката на кръга е: {circumference}");
+Console.WriteLine($"Лицето на кръга е: {area}");
